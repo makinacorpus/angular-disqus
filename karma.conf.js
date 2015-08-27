cc01b7424f787a753950ba655b923cc83aff6d2a
Sbats <simon.bats12@gmail.com>
2015-08-27 19:24:44 +0200

    update test environment

--- karma.conf.js
+++ karma.conf.js
@@ -1,21 +1,65 @@
-files = [
+// frameworks= ['jasmine'];
+// growl     = true;
+// colors    = true;
+// singleRun = true;
+// autoWatch = false;
+// browsers  = ['PhantomJS'];
+// reporters = ['progress', 'growl'];
 
-  JASMINE,
-  JASMINE_ADAPTER,
+// Karma configuration
+// http://karma-runner.github.io/0.10/config/configuration-file.html
 
-  'components/jquery/jquery.js',
-  'components/angular/angular.js',
-  'components/angular-mocks/angular-mocks.js',
+module.exports = function(config) {
+  config.set({
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
 
-  // The library itself
-  'src/angular-disqus.js',
+    // testing framework to use (jasmine/mocha/qunit/...)
+    frameworks: ['jasmine'],
 
-  'test/**.spec.js'
-];
+    // list of files / patterns to load in the browser
+    files: [
+      'components/jquery/jquery.js',
+      'components/angular/angular.js',
+      'components/angular-mocks/angular-mocks.js',
 
-growl     = true;
-colors    = true;
-singleRun = true;
-autoWatch = false;
-browsers  = ['PhantomJS'];
-reporters = ['progress', 'growl'];
+      // The library itself
+      'src/angular-disqus.js',
+
+      'test/**.spec.js'
+    ],
+
+    // list of files / patterns to exclude
+    exclude: [],
+
+    // web server port
+    port: 8080,
+
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+    colors: true,
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['PhantomJS'],
+
+    reporters: ['progress', 'growl'],
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: true
+  });
+};
